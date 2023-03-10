0507a6ffe8f61c2045f12f44b9ee48f8efe14c0e
lucaassp <lucaspereira.student@gmail.com>
2023-03-10 16:55:42 -0300

    Domain Category created and BD-H2 added

---
+++ src/main/java/com/api/catalog/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.api.catalog.repositories;
+
+import com.api.catalog.domains.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
---
+++ src/main/java/com/api/catalog/resources/CategoryResource.java
@@ -0,0 +1,32 @@
+package com.api.catalog.resources;
+
+import com.api.catalog.domains.Category;
+import com.api.catalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController // Web resource that is implemented by a Rest controller
+@RequestMapping(value = "/categories") // Resource name
+public class CategoryResource {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping // Responds to a Get resource from http
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
