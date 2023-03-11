1654389a157fd27344c8a2aa7861c91124082eba
lucaassp <lucaspereira.student@gmail.com>
2023-03-11 20:45:53 -0300

    Created method insert category

--- src/main/java/com/api/catalog/services/CategoryService.java
+++ src/main/java/com/api/catalog/services/CategoryService.java
@@ -32,5 +32,12 @@ public class CategoryService {
                 .orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
 
+        return new CategoryDTO(entity);
+    }
 }
